de904beef71a0cf911340126822ee9c3b0afb66e
yakoub <yakoubbenaissa7@hotmail.com>
2024-12-31 16:44:53 +0100

    Modified Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Home from "./Pages/Home";
+import Product from "./Pages/Product";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
    <BrowserRouter>
-    <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/product" element={<Home />} />
-          <Route path="/category" element={<Home />} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/category" element={<Home />} />
         
         </Routes>
     </BrowserRouter>
